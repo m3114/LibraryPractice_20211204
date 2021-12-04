897a7270c0c65927ca497f94bba8528c78a44f1c
윤성노 <m3114@naver.com>
2021-12-04 15:28:00 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.neppplus.librarypractice_20211204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValuse()
+    }
+
+    fun setupEvents(){
+
+    }
+    fun setValuse(){
+
+    }
+}
